30da7d84542492351112b9db9ad9d07d55d551e8
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-12-16 13:11:06 -0800

    Done

--- src/db/fake_auth.js
+++ src/db/fake_auth.js
@@ -1,8 +1,8 @@
 export function auth() {
 	return {
-		id: 'asdfasdfa',
-		name: 'Scott',
-		email: 'scott@leveluptuts.com',
+		id: '007',
+		name: 'Tim Smith',
+		email: 'tim.smith.hdg@gmail.com',
 		roles: ['admin']
 	};
 }
